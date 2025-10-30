f80fcd749aac9a86d965ff6b6b8e25e36a5a0b66
Ryan Killeen <ryan@ryankilleen.com>
2025-10-30 15:36:21 -0400

    feat: add thread-safe, global state

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
-use rocket::serde::json::Json;
+use std::sync::RwLock;
+
+use rocket::{State, serde::json::Json};
 use serde::{Deserialize, Serialize};
 
 #[macro_use]
 extern crate rocket;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 enum Category {
     Kitchen,
     Toiletries,
     Pharmacy,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 enum Store {
     BigBox,
     Grocery,
     Convenience,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 struct ListItem {
     needed: bool,
     label: String,
@@ -26,19 +28,42 @@ struct ListItem {
     stores: Vec<Store>,
 }
 
-#[get("/")]
-fn index() -> Json<Vec<ListItem>> {
-    let hmm = vec![ListItem {
-        needed: true,
-        label: "Peanut Butter".to_string(),
-        category: Category::Kitchen,
-        stores: vec![Store::BigBox, Store::Grocery],
-    }];
+type SharedList = RwLock<Vec<ListItem>>;
 
-    Json(hmm)
+#[get("/")]
+fn get_items(list: &State<SharedList>) -> Json<Vec<ListItem>> {
+    let items = list.read().expect("lock poisoned");
+
+    Json(items.clone())
+}
+
+#[post("/", data = "<item>")]
+fn add_item(list: &State<SharedList>, item: Json<ListItem>) -> &'static str {
+    let mut items = list.write().expect("lock poisoned");
+    items.push(item.into_inner());
+    "Item added"
+}
+
+#[patch("/<label>")]
+fn toggle_item(list: &State<SharedList>, label: &str) -> &'static str {
+    let mut items = list.write().expect("lock poisoned");
+
+    if let Some(existing) = items.iter_mut().find(|i| i.label == label) {
+        existing.needed = !existing.needed;
+        "Item toggled"
+    } else {
+        "Item not found"
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .manage(RwLock::new(vec![ListItem {
+            needed: true,
+            label: "Peanut Butter".to_string(),
+            category: Category::Kitchen,
+            stores: vec![Store::BigBox, Store::Grocery],
+        }]))
+        .mount("/", routes![get_items, add_item, toggle_item])
 }
